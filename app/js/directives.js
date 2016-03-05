e394b926fdd0e0def3fafbf56bc4fce88789c13b
al3xbusk <alexbusk@ukr.net>
2016-03-05 18:43:10 +0200

    progress bar edited

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,13 +1,17 @@
 'use strict';
 
 /* Directives */
-var b1 = 0;
-icrackedApp.directive("btnBar",function(){
+//var b1 = 0;
+icrackedApp.directive("btnBar0",function(){
 	return {
 		scope:{},
 		restrict: "A",
 		link: function(scope,elem,attrs) {
 			var bar = angular.element(document.querySelector('#bar'));
+			$(elem).add(function() {
+				var b1=0;
+				bar.css({'width':b1+'%'})
+			})
 			$(elem).click(function() {
 				b1+=10;
 				bar.css({'width':b1+'%'})
@@ -15,16 +19,55 @@ icrackedApp.directive("btnBar",function(){
 		}
 	}
 });
-icrackedApp.directive("btnDecr",function(){
+icrackedApp.directive("btnBar10",function(){
 	return {
 		scope:{},
 		restrict: "A",
 		link: function(scope,elem,attrs) {
 			var bar = angular.element(document.querySelector('#bar'));
+			$(elem).add(function() {
+				var b1=10;
+				bar.css({'width':b1+'%'})
+			})
 			$(elem).click(function() {
-				b1= b1-10;
+				b1+=10;
 				bar.css({'width':b1+'%'})
 			})
 		}
 	}
 });
+icrackedApp.directive("btnBar20",function(){
+	return {
+		scope:{},
+		restrict: "A",
+		link: function(scope,elem,attrs) {
+			var bar = angular.element(document.querySelector('#bar'));
+			$(elem).add(function() {
+				var b1=20;
+				bar.css({'width':b1+'%'})
+			})
+			$(elem).click(function() {
+				b1+=10;
+				bar.css({'width':b1+'%'})
+			})
+		}
+	}
+});
+icrackedApp.directive("btnBar30",function(){
+	return {
+		scope:{},
+		restrict: "A",
+		link: function(scope,elem,attrs) {
+			var bar = angular.element(document.querySelector('#bar'));
+			$(elem).add(function() {
+				var b1=30;
+				bar.css({'width':b1+'%'})
+			})
+			$(elem).click(function() {
+				b1+=10;
+				bar.css({'width':b1+'%'})
+			})
+		}
+	}
+});
+
